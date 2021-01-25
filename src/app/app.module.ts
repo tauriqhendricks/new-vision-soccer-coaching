fe0fd08751ead4144e5774476317eba31d21015c
Tauriq Hendricks <tauriqh786@gmail.com>
2021-01-25 16:03:36 +0200

    Tauriq: created the website layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeroComponent } from './layout/components/hero/hero.component';
+import { NavbarComponent } from './layout/components/navbar/navbar.component';
+import { TrainingComponent } from './layout/components/training/training.component';
+import { FeaturesComponent } from './layout/components/features/features.component';
+import { InformationComponent } from './layout/components/information/information.component';
+import { ContactComponent } from './layout/components/contact/contact.component';
+import { FooterComponent } from './layout/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    NavbarComponent,
+    TrainingComponent,
+    FeaturesComponent,
+    InformationComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
