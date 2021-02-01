d5b7ce9d14cb68dbc47d3c1105a6ec7e49dc9a0b
Tauriq Hendricks <tauriqh786@gmail.com>
2021-02-01 20:33:27 +0200

    TH: Cleaned up code

--- src/app/layout/components/contact/contact.component.ts
+++ src/app/layout/components/contact/contact.component.ts
@@ -12,31 +12,5 @@ export class ContactComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  // sendEmail() {
-  //   const subject = 'Code';
-  //   const code = '123'
-  //   const mimeData = [
-  //     'From: from@gmail.com',
-  //     'To: to@gmail.com',
-  //     'To: to@yahoo.com',
-  //     'Subject: =?utf-8?B?" + window.btoa(unescape(encodeURIComponent(subject))) + "?=',
-  //     'MIME-Version: 1.0',
-  //     'Content-Type: text/plain; charset=UTF-8',
-  //     'Content-Transfer-Encoding: 7bit',
-  //     '',
-  //     'Your code is ' + code].join('\n').trim();
-
-  //   const raw = window.btoa(unescape(encodeURIComponent(mimeData))).replace(/\+/g, '-').replace(/\//g, '_');
-
-  //   gapi.client.gmail.users.messages.send({
-  //     'userId': 'me',
-  //     'resource': {
-  //       'raw': raw
-  //     }
-  //   }).execute(res => {
-  //     console.log('Email sent', res);
-  //     // this.snackBar.success('Email has send Successfully')
-  //   });
-  // }
 
 }
