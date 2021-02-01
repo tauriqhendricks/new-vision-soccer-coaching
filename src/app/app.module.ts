ef0da6617ac632577755c34b75b543929a9bc7ee
Tauriq Hendricks <tauriqh786@gmail.com>
2021-02-01 22:14:41 +0200

    TH: Recfactor resonsiveness w/o nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,17 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeroComponent } from './layout/components/hero/hero.component';
 import { TrainingComponent } from './layout/components/training/training.component';
-import { FeaturesComponent } from './layout/components/features/features.component';
 import { InformationComponent } from './layout/components/information/information.component';
 import { ContactComponent } from './layout/components/contact/contact.component';
 import { FooterComponent } from './layout/components/footer/footer.component';
+import { QuoteComponent } from './layout/components/quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroComponent,
     TrainingComponent,
-    FeaturesComponent,
+    QuoteComponent,
     InformationComponent,
     ContactComponent,
     FooterComponent,
